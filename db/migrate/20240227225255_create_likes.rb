6b2032e369db48471906afeea8d23bcac539f3bc
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-02-28 01:05:34 +0000

    Generated comments

--- db/migrate/20240227225255_create_likes.rb
+++ db/migrate/20240227225255_create_likes.rb
@@ -3,7 +3,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
     create_table :likes do |t|
       t.references :fan, null: false, foreign_key: { to_table: :users}
       t.references :photo, null: false, foreign_key: true
-
+      t.text :body, null: false
       t.timestamps
     end
   end

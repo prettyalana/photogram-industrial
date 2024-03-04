c4e3041948319ffa2c5af70786860834208e4393
Alana Edwards <129349619+prettyalana@users.noreply.github.com>
2024-03-04 04:54:19 +0000

    Added links to Users posts and liked photos

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
   resources :photos
   # resources :users, only: :show
 
+  get ":username/liked" => "users#liked", as: :liked
+
   get "/:username" => "users#show", as: :user
 end
 
